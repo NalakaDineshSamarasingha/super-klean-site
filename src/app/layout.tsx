89fcb490298907921da4308721f5fd2ab16519c9
Nalaka Dinesh <nalakadineshx@gmail.com>
2025-11-16 11:46:55 +0530

    add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Teko } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,29 @@ export default function RootLayout({
         <AuthProvider>
           <Navbar />
           {children}
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#000',
+                color: '#fff',
+                border: '1px solid #FF5733',
+              },
+              success: {
+                iconTheme: {
+                  primary: '#FF5733',
+                  secondary: '#fff',
+                },
+              },
+              error: {
+                iconTheme: {
+                  primary: '#ff0000',
+                  secondary: '#fff',
+                },
+              },
+            }}
+          />
         </AuthProvider>
       </body>
     </html>
